26e5932c10d0b9009c79fb50337231637867efd1
JSeam2 <joshia_seam@mymail.sutd.edu.sg>
2018-03-11 23:43:40 +0800

    refactor renamed transactions.html to send_cert.html

--- ethcert_blockchain/main_web_portal/urls.py
+++ ethcert_blockchain/main_web_portal/urls.py
@@ -9,5 +9,5 @@ urlpatterns=[
     url(r'^register/$',views.register,name='register'),
     url(r'^user_login/$',views.user_login,name='user_login'),
     url(r'^user_logout/$',views.user_logout,name='user_logout'),
-    url(r'^transaction/$',views.transaction,name='transaction'),
+    url(r'^send_cert/$',views.send_cert,name='send_cert'),
 ]
