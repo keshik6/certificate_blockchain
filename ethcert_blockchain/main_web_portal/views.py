1e8fe604a035994c933612000f33fed03b8de3a2
keshik6 <keshik6@gmail.com>
2018-04-03 22:43:24 +0800

    Registration stage 1 with smtp completed
    Now after registering the user can login to do verification

--- ethcert_blockchain/main_web_portal/views.py
+++ ethcert_blockchain/main_web_portal/views.py
@@ -23,26 +23,17 @@ def user_logout(request):
 
 
 def register(request):
-    print("trying to register yo")
     registered = False
 
     if request.method == 'POST':
         # Get info from "both" forms
         # It appears as one form to the user on the .html page
         user_form = UserForm(data=request.POST,label_suffix='')
-        profile_form = UserProfileInfoForm(data=request.POST)
+
         #print(user_form.cleaned_data)
-        email = EmailMessage(
-                        'Ethcert Verification Code',
-                        'content_message',
-                        'sender smtp gmail' +'<sender@gmail.com>',
-                        ['keshik6@gmail.com'],
-                        headers = {'Reply-To': 'contact_email@gmail.com' }
-                    )
-        email.send()
 
         # Check to see both forms are valid
-        if user_form.is_valid() and profile_form.is_valid():
+        if user_form.is_valid():
 
             # Save User Form to Database
             user = user_form.save()
@@ -56,40 +47,52 @@ def register(request):
             # Now we deal with the extra info!
 
             # Can't commit yet because we still need to manipulate
-            profile = profile_form.save(commit=False)
+            #profile = profile_form.save(commit=False)
 
             # Set One to One relationship between
             # UserForm and UserProfileInfoForm
-            profile.user = user
+            #profile.user = user
 
             # Check if they provided a profile picture
-            if 'profile_pic' in request.FILES:
-                print('found it')
-                # If yes, then grab it from the POST form reply
-                profile.profile_pic = request.FILES['profile_pic']
+            # if 'profile_pic' in request.FILES:
+            #     print('found it')
+            #     # If yes, then grab it from the POST form reply
+            #     profile.profile_pic = request.FILES['profile_pic']
 
             # Now save model
-            profile.save()
+            #profile.save()
 
             #print("Profile auth code 1 is : " + str(profile.getVerificationCode1()));
 
+            print('Successfully registered')
             # Registration Successful!
             registered = True
 
+            email = EmailMessage(
+                            'ETHCERT Verification Code',
+                            'Hi ' + str(user.username) + 'Your verification code is 1234',
+                            'sender smtp gmail' +'<sender@gmail.com>',
+                            [user.email],
+                            headers = {'Reply-To': 'contact_email@gmail.com' }
+                        )
+            email.send()
+            return render(request,'main_web_portal/registration.html',
+                                  {'username':user.username, 'email':user.email,
+                                   'registered':registered})
+
         else:
             # One of the forms was invalid if this else gets called.
-            print(user_form.errors,profile_form.errors)
+            print(user_form.errors)
 
     else:
         # Was not an HTTP post so we just render the forms as blank.
         user_form = UserForm()
-        profile_form = UserProfileInfoForm()
+        #profile_form = UserProfileInfoForm()
 
     # This is the render and context dictionary to feed
     # back to the registration.html file page.
     return render(request,'main_web_portal/registration.html',
                           {'user_form':user_form,
-                           'profile_form':profile_form,
                            'registered':registered})
 
 
